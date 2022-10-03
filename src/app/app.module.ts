0927d3590fd9f59bfba03aea72bc721948ac1456
emanoelcampos <emanoel.cv@gmail.com>
2022-10-03 14:12:43 -0300

    primeiro-projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+import { MeuPrimeiro } from './meu-primeiro/meu-primeiro.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { MeuPrimeiro3Component } from './meu-primeiro3/meu-primeiro3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiro,
+    MeuPrimeiro2Component,
+    MeuPrimeiro3Component
   ],
   imports: [
     BrowserModule,
